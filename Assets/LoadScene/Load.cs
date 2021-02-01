4a2d42cdb9300bee32ae335b681d646b208aa264
Shiv <jerrytyagi4@gmail.com>
2021-02-01 16:33:17 +0530

    Initial commit

---
+++ Assets/LoadScene/Load.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Load : MonoBehaviour
+{
+    // Loading settings Scene
+    public void LoadSettings()
+    {
+        SceneManager.LoadScene(2);
+    }
+    public void back()
+    {
+        SceneManager.LoadScene(1);
+    }
+    public void Rules()
+    {
+        SceneManager.LoadScene("Rules");
+    }
+    public void LogOut()
+    {
+        SceneManager.LoadScene(0);
+    }
+    public void Setting()
+    {
+        SceneManager.LoadScene("Setting");
+    }
+    public void Shop()
+    {
+        SceneManager.LoadScene("Shop");
+    }
+    public void Play()
+    {
+        SceneManager.LoadScene("Play");
+    }
+}
